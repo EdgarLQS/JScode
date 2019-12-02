4d700f2748a4e0bf5825c027c658372735c63b06
751232541@qq.com <751232541@qq.com>
2019-12-02 15:32:46 +0800

    增加前端选中多个文件传输到后端（通过读取文件为流形式，转为hex进行传输）

--- js.js
+++ js.js
@@ -13,15 +13,68 @@ function creatTable() {
     for (var i = 0; i < rows; i++) {
         tab += '<tr>'
         for (var j = 0; j < clos; j++) {
-                // target 当点击时在另外的页面打开
-                tab += "<td ><label style=\"color:blue; text-align: center;\" class=\"showclass\" id=\"show" + i + j + "\">" + i + "</label></td>"
-                // 此处可采用 a 标签定义链接
+            // target 当点击时在另外的页面打开
+            tab += "<td ><label style=\"color:blue; text-align: center;\" class=\"showclass\" id=\"show" + i + j + "\">" + i + "</label></td>"
+            // 此处可采用 a 标签定义链接
         }
         tab += '</tr>';
     }
     tab += '</table>';
     // 显示表格
-    div1.innerHTML=tab
+    div1.innerHTML = tab
+}
+
+/**
+ * 将前端选中的文件传输到后端
+ */
+function tranferFileToBack() {
+    var objFiles = document.getElementById("btn_getFilePath");
+    var fileSize = objFiles.files.length;
+
+    for (var i = 0; i < fileSize; i++) {
+        var isFileValide = true;    // 交互click和ajax之间的信息
+        var reader = new FileReader();
+        // reader.readAsBinaryString(objFiles.files[i]);//读取文件 
+        // reader.readAsDataURL(objFiles.files[i]); // 读取为 base64 格式
+        reader.readAsArrayBuffer(objFiles.files[i]); // 读取为字节数组
+
+        var filename = objFiles.files[i].name;
+        var index = filename.lastIndexOf(".");
+        var suffix = filename.substr(index + 1);
+
+        reader.onload = function (evt) {
+            var temp = evt.target.result;
+            console.log(temp);
+            var params = buf2hex(temp);
+            console.log(params);
+
+            var postData = {
+                "data": params,
+                "fileNameExtream": suffix
+            };
+            //  添加ajax 请求，将数据返回到后端
+            $.ajax({
+                type: 'post',
+                contentType: "application/json", //后端Jersey采用相应的接受参数即可
+                url: 'http://localhost:8899/bcgis/storage/cooperativeStorage/test',
+                data: JSON.stringify(postData),
+                success: function (data) {
+                    // console.log('data: ' + JSON.stringify(data));
+                    console.log(data);
+                },
+                error: function (err) {
+                    console.log('err: ');
+                    console.log(JSON.stringify(err));
+                }
+            });
+
+        }
+    }
+}
+
+//arraybuffer to hex
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
 
